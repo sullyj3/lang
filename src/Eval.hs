74da3e6ece98fea14062e387edc3056a50430dd1
James Sully <sullyj3@gmail.com>
2022-12-10 22:14:03 +1100

    wip

--- src/Eval.hs
+++ src/Eval.hs
@@ -4,10 +4,14 @@ import Data.Function (on)
 import Control.Monad
 import Control.Monad.Trans.Reader
 import Control.Monad.Trans.Class (lift)
+import Control.Monad.Error
 import Control.Applicative
 
 import Data.Text (Text)
 import Data.Text qualified as T
+
+import Data.Map (Map)
+import Data.Map qualified as Map
 
 import Expr
 -- import Debug.Trace (traceM, trace)
@@ -15,7 +19,7 @@ import Expr
 -- TODO use Data.Map
 type Env = [Binding]
 
-data RuntimeError = UndefinedVariable Var | TypeError Text | MainNotFound
+data RuntimeError = UndefinedVariable Var | DynTypeError Text | MainNotFound
   deriving (Show, Eq)
 
 data Value = VL Var Expr | VI Int | VStr Text
@@ -46,13 +50,54 @@ eval e = do
       fn <- eval e1
       case fn of
         VL v lamBody -> local ((v, e2) :) $ eval lamBody
-        _ -> runtimeError $ TypeError $ "Cannot apply " <> tShow fn <> " to " <> tShow e2
+        _ -> runtimeError $ DynTypeError $ "Cannot apply " <> tShow fn <> " to " <> tShow e2
   where 
     liftEval :: (Value -> Value -> Either RuntimeError Value) 
              -> (Expr  -> Expr  -> Eval Value)
     liftEval op e1 e2 =
       joinError $ (liftA2 op `on` eval) e1 e2
 
+data Type = LitT LitType | LamT Type Type | Unknown
+  deriving (Show, Eq)
+
+data LitType = IntT | StringT
+  deriving (Show, Eq)
+
+data TypeError = TypeError Expr Type Type
+               | UnknownType Type Expr
+  deriving (Show, Eq)
+
+-- TODO read TAPL and come back to this
+-- uniTypeCheck :: Type -> Expr -> TypeCheck ()
+-- uniTypeCheck expected expr@(V v) = do
+--   env <- ask
+--   case lookup v env of
+--     Just actual -> expect expected actual expr
+--     Nothing -> throwError $ UnknownType ty expr
+-- uniTypeCheck expected expr@(LitInt _) = expect expected (LitT IntT) expr
+-- uniTypeCheck expected expr@(LitString _) = expect expected (LitT StringT) expr
+-- uniTypeCheck expected expr@(Binop Plus l r) = do
+--   expect expected (LamT (LitT IntT) (LamT (LitT IntT) (LitT IntT))) expr
+--   uniTypeCheck (LitT IntT) l
+--   uniTypeCheck (LitT IntT) r
+-- uniTypeCheck expected expr@(Binop Minus l r) = do
+--   expect expected (LamT (LitT IntT) (LamT (LitT IntT) (LitT IntT))) expr
+--   uniTypeCheck (LitT IntT) l
+--   uniTypeCheck (LitT IntT) r
+-- -- TODO ensure we're handling shadowing correctly
+-- uniTypeCheck (LamT paramT bodyT) (Lam v lamBody) = local (Map.insert v paramT) $
+--   uniTypeCheck bodyT lamBody
+-- uniTypeCheck expected expr@(Lam _ _) 
+--   = throwError $ TypeError expr expected (LamT Unknown Unknown)
+-- uniTypeCheck expected (App f x) = undefined
+
+expect :: Type -> Type -> Expr -> TypeCheck ()
+expect expected actual expr
+  | expected == actual = pure ()
+  | otherwise = throwError $ TypeError expr expected actual
+
+type TypeCheck a = ReaderT TypeEnv (Either TypeError) a
+type TypeEnv = Map Var Type
 
 -- counterexample:
 -- (|x -> |y -> x + y) 1 2
@@ -81,11 +126,11 @@ tShow = T.pack . show
 
 valuePlus :: (Value -> Value -> Either RuntimeError Value)
 valuePlus (VI a) (VI b) = Right $ VI $ a + b
-valuePlus a b = Left . TypeError $ "cannot add " <> tShow a <> " and " <> tShow b 
+valuePlus a b = Left . DynTypeError $ "cannot add " <> tShow a <> " and " <> tShow b 
 
 valueMinus :: (Value -> Value -> Either RuntimeError Value)
 valueMinus (VI a) (VI b) = Right $ VI $ a - b
-valueMinus a b = Left . TypeError $ "cannot subtract " <> tShow a <> " and " <> tShow b 
+valueMinus a b = Left . DynTypeError $ "cannot subtract " <> tShow a <> " and " <> tShow b 
 
 runtimeError :: RuntimeError -> Eval a
 runtimeError = lift . Left
@@ -97,6 +142,6 @@ evalIt :: Expr -> Either RuntimeError Value
 evalIt e = runReaderT (eval e) []
 
 joinError :: Eval (Either RuntimeError a) -> Eval a
-joinError = join . fmap lift
+joinError = (lift =<<)
 
   
